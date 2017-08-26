adbebe1fb9130dbcc3b1ed119cd1f50e996fa2e2
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2017-08-26 21:32:27 +0200

    Colisionador de croquetas y game manager

---
+++ Assets/Scripts/ColisionadorCroquetas.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColisionadorCroquetas : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	/// <summary>
+	/// OnTriggerEnter is called when the Collider other enters the trigger.
+	/// </summary>
+	/// <param name="other">The other Collider involved in this collision.</param>
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.CompareTag("croquetas")){
+			//Eliminamos croqueta
+			Destroy(other.gameObject);
+			//Sumamos puntos
+			ManagerCroquetas.instance.puntos ++;
+			//creamos otra croqueta
+
+		}
+		
+	}
+
+
+}
---
+++ Assets/Scripts/ManagerCroquetas.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerCroquetas : MonoBehaviour {
+
+
+	public static ManagerCroquetas instance { get; private set; }
+	public int puntos;
+	public List<Transform> zonasSpawnCroquetas;
+	public List<Transform> zonaObjetivos;
+
+
+
+	void Awake() {
+		if(instance == null) {
+			instance = this;
+		} else if(instance != this) {
+			Destroy(this.gameObject);
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
