d78c0de8cac1aed60033a17f1d1cd2fd7fa22fe0
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2017-08-27 17:58:29 +0200

    main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+
+
+
+	public static MainMenu instance { get; private set; }
+
+	public GameObject pug;
+
+	void Awake() {
+		if(instance == null) {
+			instance = this;
+		} else if(instance != this) {
+			Destroy(this.gameObject);
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
+	// Use this for initialization
+	void Start () {
+
+		pug.GetComponent<Animation>().Play();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void IniciaJuego(){
+		SceneManager.LoadScene(1);
+		
+
+	}
+
+
+}
